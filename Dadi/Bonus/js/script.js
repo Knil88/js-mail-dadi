a4989ecfc9f6856ee4a1ec0798c1ba03b7b57bd0
Yari <narducciyarigmail.com>
2022-11-10 15:41:21 +0100

    modifica grafica dadi

--- Dadi/Bonus/js/script.js
+++ Dadi/Bonus/js/script.js
@@ -6,21 +6,27 @@ let userdice = Math.floor(Math.random()* 5) + 1;
 
 let pcdice = Math.floor(Math.random()* 5) + 1;
 
+const myButton = document.addEventListener("click",
+function (event){
+   
+    if(userdice > pcdice){
+        console.log("Complimenti hai vinto")
+        document.getElementById("title").innerHTML = `Complimenti hai vinto`;
+   }
+   if(userdice < pcdice){
+       console.log("Hai perso")
+       document.getElementById("title").innerHTML = `Spiacente hai perso`;
+   }
+   else if(userdice === pcdice){
+   
+       console.log("pareggio")
+       document.getElementById("title").innerHTML = `Pareggio`;
+   
+   }
+}
+)
+
 
 
 console.log(userdice,pcdice);
 
-if(userdice > pcdice){
-     console.log("Complimenti hai vinto")
-     document.getElementById("title").innerHTML = `Complimenti hai vinto`;
-}
-if(userdice < pcdice){
-    console.log("Hai perso")
-    document.getElementById("title").innerHTML = `Spiacente hai perso`;
-}
-else if(userdice === pcdice){
-
-    console.log("pareggio")
-    document.getElementById("title").innerHTML = `Pareggio`;
-
-}
